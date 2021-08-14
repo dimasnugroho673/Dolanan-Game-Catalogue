dd5bf6eb100056ee6557083b330b679c10d1c25d
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-08-14 12:10:40 +0700

    Initial Commit

---
+++ Dolanan.id Game Catalogue/Dolanan_id_Game_CatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dolanan_id_Game_CatalogueApp.swift
+//  Dolanan.id Game Catalogue
+//
+//  Created by Dimas Putro on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Dolanan_id_Game_CatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
