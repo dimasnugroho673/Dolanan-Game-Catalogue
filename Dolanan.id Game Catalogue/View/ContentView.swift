999b8c5eb4496adcc0dee4ff72e267f32ce8b597
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-08-26 12:39:28 +0700

    nambahin contentview

--- Dolanan.id Game Catalogue/View/ContentView.swift
+++ Dolanan.id Game Catalogue/View/ContentView.swift
@@ -8,38 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Home")
-                }
-            
-            Text("Genre")
-                .tabItem {
-                    Image(systemName: "puzzlepiece")
-                    Text("Genre")
-                }
-            
-            SearchView()
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search")
-                }
-            
-            Text("More")
-                .tabItem {
-                    Image(systemName: "ellipsis.circle")
-                    Text("More")
-                }
-        }
+  
+  @State private var isUserExist = UserDefaults.standard.bool(forKey: "UserExist")
+  @State private var isOnboardingPresent = true
+  
+  var body: some View {
+    if isUserExist {
+      TabView {
+        HomeView()
+          .tabItem {
+            Image(systemName: "house")
+            Text("Home")
+          }
         
+        GenreView()
+          .tabItem {
+            Image(systemName: "puzzlepiece")
+            Text("Genre")
+          }
+        
+        SearchView()
+          .tabItem {
+            Image(systemName: "magnifyingglass")
+            Text("Search")
+          }
+        
+        GameBookmarkView()
+          .tabItem {
+            Image(systemName: "bookmark")
+            Text("bookmark")
+          }
+      }
+    } else {
+      VStack {
+        
+      }
+      .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+      .background(Color.white)
+      .edgesIgnoringSafeArea(.all)
+      .fullScreenCover(isPresented: $isOnboardingPresent, content: {
+        OnboardingView(isUserExist: $isUserExist)
+      })
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  }
 }
