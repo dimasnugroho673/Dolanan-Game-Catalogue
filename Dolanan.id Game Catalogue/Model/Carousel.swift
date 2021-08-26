2b44e2ddcdbf8c27b2c40a7ce600f6ae3ce2130d
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-08-26 11:52:36 +0700

    nambahin carouselshow model

---
+++ Dolanan.id Game Catalogue/Model/Carousel.swift
@@ -0,0 +1,14 @@
+//
+//  Carousel.swift
+//  Dolanan.id Game Catalogue
+//
+//  Created by Dimas Putro on 24/08/21.
+//
+
+import Foundation
+
+struct CarouselShow {
+  let id: Int
+  let title: String
+  let image: String
+}
