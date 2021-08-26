a40b303289f1d0f4fed6e587ccf5476d2f4a6d51
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-08-26 11:47:35 +0700

    nambahin keyword api pada exept rule pada swiftlint.yml, dan ubah constant dan file root project menjadi camelCase dan PascalCase

--- Dolanan.id Game Catalogue/DolananIdGameCatalogueApp.swift
+++ Dolanan.id Game Catalogue/DolananIdGameCatalogueApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct Dolanan_id_Game_CatalogueApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct DolananIdGameCatalogueApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
